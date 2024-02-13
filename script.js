23b72e6f88b664cadaddcb450639fd38a31ccaef
Yuvraj Maurya <yuvraj95166@gmail.com>
2024-02-13 22:58:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+ function setBackgroundImage() {
+        var backgroundImageUrl;
+        
+        if (window.innerHeight > window.innerWidth) {
+            // Portrait mode
+            backgroundImageUrl = "https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg";
+        } else {
+            // Landscape mode
+            backgroundImageUrl = "https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg";
+        }
+        
+        document.body.style.backgroundImage = "url('" + backgroundImageUrl + "')";
+    }
+    
+    // Call the function initially
+    setBackgroundImage();
+    
+    // Call the function whenever the window is resized
+    window.addEventListener("resize", setBackgroundImage);
